82f980be8f2e54d43f4677172b76bea0d0bf232c
lilith-t <LilithThomson1205@gmail.com>
2025-12-15 04:03:16 +0000

    added jumping, camera movement, shooting
    added camera movement as well as jumping as well as shooting

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,24 +1,35 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     private PlayerInput playerInput;
     private PlayerInput.OnGroundActions onGround;
     private PlayerMovement movement;
+    private PlayerLook playerlook;
+    private PlayerShooty playerShooty;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         playerInput = new PlayerInput();
         onGround = playerInput.OnGround;
         movement = GetComponent<PlayerMovement>();
+        playerlook = GetComponent<PlayerLook>();
+        playerShooty = GetComponent<PlayerShooty>();
+        onGround.Jump.performed += ctx => movement.Jump();
         
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         movement.Movement(onGround.movement.ReadValue<Vector2>());
+        playerlook.Looking(onGround.Look.ReadValue<Vector2>());
+        if (onGround.Shoot.IsPressed()) 
+            {
+            playerShooty.Shoot();
+            }
     }
     private void OnEnable()
     {
